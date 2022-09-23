3e80a28f7884937c2b7269f7b8ab03da5ed92729
daguerre33 <92821707+daguerre33@users.noreply.github.com>
2022-09-23 21:30:42 +0200

    Create clickjacking_tester2.py

---
+++ clickjacking_tester2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from requests_html import HTMLSession
+
+session = HTMLSession()
+url = input("Url (eg. http://www.example.com): ")
+response = session.get(url)
+content = response.headers
+if content.__contains__("X-Frame-Options"):
+        print("iframe is not possible")
+else:
+        print("iframing is possible!")
+
